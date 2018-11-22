97985f48b26b8c3e052aa535b9a52a7ba90d2fb5
Roy <royabel10@gmail.com>
2018-11-22 11:46:33 +0200

    adding graph recall per % known data

--- roy/main.py
+++ roy/main.py
@@ -10,18 +10,17 @@ from graph_measures import feature_meta
 import matplotlib.pyplot as plt
 
 CHOSEN_FEATURES = feature_meta.NODE_FEATURES
+labels = ['cora']
+# labels = ['Adapter', 'Ligand', 'Vesicles', 'Ribosomes', 'Membrane']
+Results = {}
 
 
 class LoadData:
     def __init__(self):
-        self._data_dir = os.path.join("data_sets", "moreno_blogs")
-        # self._data_dir = "signaling_pathways"
-        self._label = "moreno"
+        self._data_dir = os.path.join("data_sets", "subelj_cora")
+        self._label = "cora"
         df1 = pd.read_csv(os.path.join(self._data_dir, "graph_edges.txt"))
-        # df1 = pd.read_csv(os.path.join(self._data_dir, "signaling_pathways_2004.txt"))
         self._gnx = nx.from_pandas_edgelist(df1, "n1", "n2", create_using=nx.DiGraph())
-        # self._gnx = nx.DiGraph()  # should be a subclass of Graph
-        # self._gnx.add_edges_from([(0, 1), (0, 2), (1, 3), (3, 2)])
         self._logger = PrintLogger("MyLogger")
         self._features_mx = None
         self._tags = {}
@@ -34,46 +33,70 @@ class LoadData:
         print(self._features_mx.shape)
 
     def build_tags(self):
-        for label in ['moreno']:
-            # ['Adapter', 'Ligand', 'Vesicles', 'Ribosomes', 'Membrane']
+        for label in labels:
             self._tags[label] = []
             with open(os.path.join(self._data_dir, "tags_" + label + ".txt"), "r") as f:
                 for node, line in enumerate(f):
+                    # for line in f:
                     # node, tag = line.split()
-                    # self._gnx.node[int(node)][label] = int(tag)
                     tag = str(line.rstrip("\n"))
+                    tag = tag[1:tag.find('/', 1)]
+                    # if not(self._gnx.has_node(node)):
+                    #     self._gnx.add_node(node)
+                    # self._gnx.node[node][label] = tag
                     self._gnx.node[node+1][label] = tag
             for node in sorted(self._gnx):
                 self._tags[label].append(self._gnx.node[node][label])
 
-    def run_eps_greedy(self, learn_type="ML"):
-        dist_calc_type = "one_class"
-        label_to_eps_to_steps = {x: {y: 0 for y in [0, 0.01, 0.05]} for x in ['moreno']}
-        # ['Adapter', 'Ligand', 'Vesicles', 'Ribosomes', 'Membrane']
-        print("using learning type: " + str(learn_type))
-        s1 = "greedy_" + learn_type + "_0_40"
-        s2 = "greedy_" + learn_type
-        for label in ['moreno']:
-            for eps in [0.01]:
-                mean_steps = 0
-                print(label, dist_calc_type)
+
+class Learning:
+    def __init__(self, features_mx, tags):
+        self._mx = features_mx
+        self._tags = tags
+
+    def run_eps_greedy(self, dist_calc_type="one_class", learn_type="ML", recall=0.7, epochs=5):
+        # finds recall achieved per percent of revealed data
+        epsilons = [0, 0.01, 0.05]
+        label_to_eps_to_steps = {x: {y: {} for y in epsilons} for x in labels}
+
+        for label in labels:
+            for eps in epsilons:
+                mean_steps_per_recall = {round(x, 2): 0 for x in np.arange(0, recall + 0.05, 0.05)}
+                print(label, dist_calc_type, learn_type)
                 time_tag_dict = {}
-                for i in range(1, 11):
-                    exploration = ExploreExploit(self._tags[label], self._features_mx, 0.7, eps)
-                    num_steps, tags, n1, n2 = exploration.run(dist_calc_type, learn_type)
-                    print(" an recall of 70% was achieved in " + str(num_steps) + " steps")
-                    mean_steps += num_steps
+                for i in range(1, epochs + 1):
+                    exploration = ExploreExploit(self._mx, self._tags[label], recall, eps)
+                    steps_per_recall, tags = exploration.run_recall(dist_calc_type, learn_type)
+                    print(" an recall of " + str(recall*100) + "% was achieved in "
+                          + str(steps_per_recall[recall]) + " steps")
+                    mean_steps_per_recall = {k: mean_steps_per_recall[k] + steps_per_recall[k]
+                                             for k in mean_steps_per_recall}
                     time_tag_dict[i] = tags
-                    Results[s1] += n1
-                    Results[s2] += n2
                 time_tag_df = pd.DataFrame.from_dict(time_tag_dict, orient="index").transpose()
                 time_tag_df.to_csv(label + "_" + dist_calc_type + "_output.csv")
-                print("the mean num of steps is: " + str(mean_steps / 10))
-                label_to_eps_to_steps[label][eps] = mean_steps / 10
-                Results[s1] /= 10
-                Results[s2] /= 10
-            df = pd.DataFrame(label_to_eps_to_steps)
-            print(df)
+
+                mean_steps_per_recall = {k: mean_steps_per_recall[k] / epochs
+                                         for k in mean_steps_per_recall}
+                print("the mean num of steps is: " + str(mean_steps_per_recall[recall]))
+                label_to_eps_to_steps[label][eps] = mean_steps_per_recall[recall]
+                Results[str(label)+'_'+str(eps)] = mean_steps_per_recall
+        df = pd.DataFrame(label_to_eps_to_steps)
+        print(df)
+
+    def run_stand_recall(self, dist_calc_type="one_class", learn_type="ML", epochs=5):
+        # finds recall achieved per percent of revealed data
+        print("finding recall using standard ml")
+        for label in labels:
+            learning = StandardML(self._mx, self._tags[label])
+            print(label, dist_calc_type, learn_type)
+            for i in range(1, epochs + 1):
+                for certainty in np.arange(0.2, 0.6, 0.1):
+                    certainty = round(certainty, 2)
+                    Results[str(label) + "_standML" + str(certainty) + "_" + str(i)] = {0: 0}
+                    for p in np.arange(0.05, 1.05, 0.05):
+                        p = round(p, 2)
+                        recall, steps = learning.recall_per_data(p, certainty)
+                        Results[str(label) + "_standML" + str(certainty) + "_" + str(i)][recall] = steps
 
     def run_al(self, learn_type="ML"):
         dist_calc_type = "one_class"
@@ -93,11 +116,12 @@ class LoadData:
                 Results['Active'] /= 10
 
     def run_standard_ml(self):
-        label = 'moreno'
+        # TODO: adding an option for multi labels
+        label = labels[0]
         sum_acc_ml = 0
         sum_acc_dl = 0
         for i in range(10):
-            learning = StandardML(self._tags[label], self._features_mx)
+            learning = StandardML(self._features_mx, self._tags[label])
             acc_ml, acc_dl = learning.run()
             print("accuracy of standard ml using random forest is: " + str(acc_ml)
                   + " for deep is: " + str(acc_dl))
@@ -111,25 +135,29 @@ class LoadData:
 
 
 def my_plot():
-    x = ['greedy_ML_0_40', 'greedy_ML', 'greedy_DL_0_40', 'greedy_DL', 'Active', 'standard_ML', 'standard_DL']
-    xs = ['ML_0_40', 'Greed_ML', 'DL_0_40', 'Greed_DL', 'Active' 'stand_ML', 'stand_DL']
-    ys = [Results[i] for i in x]
-    plt.bar(xs, ys)
-    plt.savefig('test6.png')
-    plt.show()
+    for i in Results:
+        xs = [Results[i][x] for x in Results[i]]
+        ys = [x for x in Results[i]]
+        plt.plot(xs, ys, label=str(i))
+    plt.title("recall per time")
+    plt.legend()
+    plt.xlabel('data_revealed')
+    plt.ylabel('recall')
+    fig = plt.gcf()
+    fig.show()
+    fig.savefig('recall_per_time.png')
 
-
-Results = {x: 0 for x in ['greedy_ML_0_40', 'greedy_ML', 'greedy_DL_0_40', 'greedy_DL', 'Active',
-                          'standard_ML', 'standard_DL']}
 
 if __name__ == '__main__':
     dl = LoadData()
     dl.build_features()
     dl.build_tags()
-    dl.run_eps_greedy("ML")
+    learn = Learning(dl._features_mx, dl._tags)
+    # learn.run_eps_greedy(learn_type="ML", recall=0.7, epochs=4)
+    learn.run_stand_recall(learn_type="ML", epochs=2)
     # dl.run_eps_greedy("DL")
-    dl.run_al("ML")
-    # dl.run_al("DL")
-    dl.run_standard_ml()
+    # dl.run_al("ML")
+    # # dl.run_al("DL")
+    # dl.run_standard_ml()
     my_plot()
     print("done")
